ce1b283f4e23d4379bfa90638b4bba4d55bf9fff
Steven Frey <94216578+StevenFrey@users.noreply.github.com>
2022-01-02 23:30:50 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+function App() {
+
+    return ( 
+        <div className = 'container' >
+
+            <h1 > Hello From React </h1> 
+            <Header />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>TaskTracker</h1>
+        </header>
+    )
+}
+
+export default Header
