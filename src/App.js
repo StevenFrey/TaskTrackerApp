cde441fbf8ed854bb3b04d0e46ac72c58c4c02df
Steven Frey <94216578+StevenFrey@users.noreply.github.com>
2022-01-08 23:41:20 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -31,7 +31,10 @@ function App() {
   // Add task
 
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 10000 + 1)
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+    
   }
 
   // delete task
@@ -55,7 +58,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
 
       {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
