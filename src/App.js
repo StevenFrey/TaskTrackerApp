9743d25f36f708b65f7666aee4468629ced03ba8
Steven Frey <94216578+StevenFrey@users.noreply.github.com>
2022-01-08 23:37:51 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 function App() {
@@ -27,11 +28,36 @@ function App() {
 
   ])
 
-  
+  // Add task
+
+  const addTask = (task) => {
+    console.log(task)
+  }
+
+  // delete task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+ 
+  // Toggle reminder
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: 
+        !task.reminder } : task
+        )
+    )
+  }
+
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks = {tasks}/>
+      <AddTask />
+
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
 
   );
